70b37639f9c8cd6a0f5340d11b3de6176a6f5002
Ed Hagen <ehagen@gmail.com>
2020-06-14 11:56:01 -0700

    Overhaul of hagenheat. Bumped version.

--- R/other_utils.R
+++ R/other_utils.R
@@ -290,19 +290,25 @@ ggemmeans <- function(em, reorder = T){
 }
 
 #' @title hagenheat
-#' @description Basic heatmap using ggplot, dist, and hclust. No dendrograms are plotted, though.
+#' @description Basic heatmap using ggplot, dist, and seriation. No dendrograms are plotted.
 #' @param d A data frame, matrix, or dist object. Column 1 of a data frame can be row labels. Remaining columns must be numeric.
 #' @param method "seriate" or "hclust". Default: "seriate"
-#' @param seriation_method Default 'Spectral'
+#' @param seriation_method If not specified, 'Spectral' will be used for dist objects, and 'PCA' for matrices and data frames.
+#' @param independent Whether to seriate a matrix using matrix methods (F) or separate distance matrices for rows and columns (T). Default: F
 #' @param hc_method Agglomeration method from hclust, Default: 'ward.D'
 #' @param dist Distance method from dist, Default: 'euclidean'
 #' @param scale. Whether to scale rows ('row'), columns ('column'), or neither ('none'), Default: 'none'
 #' @param viridis_option One of the viridis color options, 'A', 'B', 'C', 'D', 'E', Default: 'D'
+#' @param wrap_labels Logical. Wrap the x-axis labels. Default: T
+#' @param rotate_labels Logical. Rotate the x-axis labels by 90 degrees. Default: F
 #' @param ann_col A data frame with two variables. The first column must be the names of the columns of 'd'. The second column contains the annotation values.
 #' @return A ggplot object
 #' @details Produces a very simple ggplot heatmap using viridis colors. 
-#' Rows and columns are clustered using the seriation package.
-#' First column of data frame must be row labels. Remaining columns must be numeric.
+#' Rows and columns are ordered using the \code{seriation} package.
+#' For data frames, first column must be row labels. Remaining columns must be numeric.
+#' Scaling, if performed, occurs after seriation, and is therefore only for display purposes.
+#' Seriation methods for objects when \code{independent=T}: \code{list_seriation_methods('dist')}.
+#' Seriation methods for matrices and data frames when \code{independent=F}: \code{list_seriation_methods('matrix')}.
 #' @examples 
 #' \dontrun{
 #' if(interactive()){
@@ -312,79 +318,101 @@ ggemmeans <- function(em, reorder = T){
 #' @seealso 
 #'  \code{\link[seriation]{seriate}}
 #'  \code{\link[seriation]{seriation_methods}}
-#'  \code{\link[viridis]{scale_color_viridis}}
 #'  \code{\link[ggnewscale]{new_scale_fill}}
 #' @rdname hagenheat
 #' @export 
 #' @importFrom seriation seriate get_order
 #' @importFrom dplyr bind_cols
 #' @importFrom tidyr gather
-#' @importFrom viridis scale_fill_viridis
 #' @importFrom scales label_wrap
 #' @importFrom ggnewscale new_scale_fill
 hagenheat <- function(
   d, 
   method='seriate', 
-  seriation_method='Spectral', 
+  seriation_method=NULL, 
+  independent = F,
   hc_method = 'ward.D', 
   dist_method = 'euclidean', 
   scale. = "none", 
   viridis_option = 'D',
+  wrap_labels = T,
+  rotate_labels = F,
   ann_col = NULL
   ){
   
-  # Convert dist object or data frame to matrix
-  rwnms <- character(0)
-  if ('dist' %in% class(d)) {
-    d <- as.matrix(d)
-  } else if ('data.frame' %in% class(d)){
-    if (is.character(d[[1]]) | is.factor(d[[1]])){
-      rwnms <- d[[1]]
-      d <- d[-1]
-    }
-    d <- as.matrix(d)
-  }
-  
-  # If d is logical, converts to numeric
-  if (mode(d) == 'logical') d <- d*1
-  if (mode(d) != 'numeric') stop('d must be convertible to a numeric matrix')
-  
-  if (length(di <- dim(d)) != 2) 
-    stop("'d' must have 2 dimensions")
-
-  nr <- di[1L]
-  nc <- di[2L]  
-  if (nr <= 1 || nc <= 1) 
-    stop("'d' must have at least 2 rows and 2 columns")
-
-  # Get rownames if haven't gotten them already
-  if (length(rwnms) == 0){
-    if(length(rownames(d))>1){
-      rwnms <- rownames(d)
-    } else {
-      rwnms <- as.character(1:nrow(d))
-    }
-  }
-  
-  # Order the rows and columns
-  if (method == 'seriate'){
-  
-    o <- seriation::seriate(dist(d, method = dist_method), method=seriation_method)
+  if ('dist' %in% class(d)){
+    
+    rwnms <- labels(d)
+    if (is.null(seriation_method)) seriation_method <- 'Spectral'
+    o <- seriation::seriate(d, method = seriation_method)
     row_order <- seriation::get_order(o)
-    o <- seriation::seriate(dist(t(d), method = dist_method), method=seriation_method)
-    col_order <- seriation::get_order(o)
-    
-  } else if (method == 'hclust'){
-    
-    hclustrows <- hclust(dist(d, method = dist_method), method = hc_method)
-    row_order <- hclustrows$order
-    hclustcols <- hclust(dist(t(d), method = dist_method), method = hc_method)
-    col_order <- hclustcols$order
+    col_order <- row_order
+    d <- as.matrix(d)
     
   } else {
     
-    stop("method must be 'seriate' or 'hclust'")
+    # Convert data frame to matrix
+    rwnms <- character(0)
+    if ('data.frame' %in% class(d)){
+      if (is.character(d[[1]]) | is.factor(d[[1]])){
+        rwnms <- d[[1]]
+        d <- d[-1]
+      }
+      d <- as.matrix(d)
+    }
     
+    # If d is logical, converts to numeric
+    if (mode(d) == 'logical') d <- d*1
+    if (mode(d) != 'numeric') stop('d must be convertible to a numeric matrix')
+    
+    if (length(di <- dim(d)) != 2) stop("'d' must have 2 dimensions")
+    
+    nr <- di[1L]
+    nc <- di[2L]
+    
+    if (nr <= 1 || nc <= 1) stop("'d' must have at least 2 rows and 2 columns")
+    
+    # Get rownames if haven't gotten them already
+    if (length(rwnms) == 0){
+      if(length(rownames(d))>1){
+        rwnms <- rownames(d)
+      } else {
+        rwnms <- as.character(1:nrow(d))
+      }
+    }
+    
+    # Order the rows and columns
+    if (method == 'seriate'){
+      
+      if (is.null(seriation_method)) seriation_method <- 'PCA'
+      
+      if (independent){
+        
+        o <- seriation::seriate(dist(d, method = dist_method), method=seriation_method)
+        row_order <- seriation::get_order(o)
+        o <- seriation::seriate(dist(t(d), method = dist_method), method=seriation_method)
+        col_order <- seriation::get_order(o)
+        
+      } else {
+        
+        o <- seriation::seriate(d, method=seriation_method)
+        row_order <- seriation::get_order(o, dim = 1)
+        col_order <- seriation::get_order(o, dim = 2)
+        
+      }
+      
+    } else if (method == 'hclust'){
+      
+      hclustrows <- hclust(dist(d, method = dist_method), method = hc_method)
+      row_order <- hclustrows$order
+      hclustcols <- hclust(dist(t(d), method = dist_method), method = hc_method)
+      col_order <- hclustcols$order
+      
+    } else {
+      
+      stop("method must be 'seriate' or 'hclust'")
+      
+    }
   }
   
   if (scale. == 'row'){
@@ -404,12 +432,14 @@ hagenheat <- function(
     mutate(
       key = factor(key, levels = colnames(d[-1])[col_order]),
     ) %>%
-    ggplot(aes_string('key', colnames(.)[1], fill = 'value')) +
+    ggplot(aes_string('key', 'rowname', fill = 'value')) +
     geom_tile() +
-    viridis::scale_fill_viridis(option = viridis_option) +
-    scale_x_discrete(labels = scales::label_wrap(10)) +
+    scale_fill_viridis_c(option = viridis_option) +
     labs(x = "", y = "") +
     theme_minimal()
+  
+  if (wrap_labels) p <- p + scale_x_discrete(labels = scales::label_wrap(10))
+  if (rotate_labels) p <- p + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
   
   if (!is.null(ann_col)){
     
